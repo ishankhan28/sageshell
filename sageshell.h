f36373626c9a259971a8e3d8e056c9e1d8a3211a
Khateeb07 <khateeb0712@gmail.com>
2026-04-21 12:02:51 +0530

    Refactor: improved autocomplete, ghost text, and suggestion accuracy

--- sageshell.h
+++ sageshell.h
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<sys/types.h>
-#include<sys/wait.h>
-#include<unistd.h>
-#include<errno.h>
-#define MAX_BUFFER 256
-#define QUIT_STRING "quit"
-#include<fcntl.h>
-#include<sys/stat.h>
-#define FFLAG (O_WRONLY|O_CREAT|O_TRUNC)
-#define FMODE (S_IRUSR|S_IWUSR)
-void executeredirect(char*,int,int);
-int getargv(const char*,const char*,char***);
-int parseandredirectin(char*);
-int parseandredirectout(char*);
-void executecmd(char*);
-void printBanner();
-=======
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
@@ -136,6 +115,7 @@ char* getSuggestion(char*);
 void clearMatches();
 void buildMatches(char*);
 void renderGhost(char*);
+
+// Handling TAB
 void handleTab(char*, int*);
 void exitCycling();
->>>>>>> c21b8e4 (Update: added autocomplete, ghost text, and suggestion system)
